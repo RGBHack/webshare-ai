13c4a22daac97e207881ea98cee3804227bb904c
root <root@coolvm.d3acplv5zbhuhlzivqugeylrhe.xx.internal.cloudapp.net>
2020-11-21 19:51:02 +0000

    ai request done

--- src/app.py
+++ src/app.py
@@ -1,3 +1,11 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
 from flask import Flask, render_template, make_response, flash, redirect, request, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import os
@@ -5,10 +13,7 @@ import random
 import string
 
 app = Flask(__name__)
-
-# def ai(imgpath) {
-#     # for runnign model
-# }
+model = ResNet50(weights='imagenet')
 
 ALLOWED_EXTENSIONS = {"jpg"}
 upP = "../usr_data"
@@ -26,11 +31,19 @@ def pp():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         filename2 = random.choices(string.ascii_uppercase + string.digits, k=13)
-        os.rename(filename,filename2 )
+        filename2 = ''.join(filename2)
+        
         actual_file_path = os.path.join(upP, filename2)
         file.save(os.path.join(upP, filename2))
+
+        img_path = os.path.join(upP, filename2)
+        img = image.load_img(img_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        preds = model.predict(x)
+        
         os.remove(os.path.join(upP, filename2))
+        return decode_predictions(preds, top=3)[0][0][1]
 
-
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=int(os.environ.get('PORT', 5003)))
+app.run(debug=True, host='0.0.0.0', port=80)
